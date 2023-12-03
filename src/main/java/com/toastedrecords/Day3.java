3ae338c7835ff663d4c9ded7b8bb50489562c4d6
hitman <btosler@hotmail.com>
2023-12-03 17:26:11 +0000

    Day 3 prep

---
+++ src/main/java/com/toastedrecords/Day3.java
@@ -0,0 +1,11 @@
+package com.toastedrecords;
+
+import com.toastedrecords.common.InputReader;
+
+import java.util.List;
+
+public class Day3 {
+    public static void main(String[] args) {
+        List<String> input = InputReader.parseInput("day3");
+    }
+}
